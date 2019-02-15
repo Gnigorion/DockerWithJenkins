29877e931e53c3a36334266e7aadd4ec70c74b3a
VipinSati <vipin.sati@robomq.io>
2019-02-15 10:16:59 +0530

    Added the proj 1

---
+++ DockImg/src/main/java/io/vip/DockImg/App.java
@@ -0,0 +1,13 @@
+package io.vip.DockImg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from java" );
+    }
+}
